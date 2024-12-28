da9b39254ab89201511a5e470c48ae4527388480
umeshR10 <urahangdale2000@gmail.com>
2024-12-28 11:39:24 +0530

    adding portfolio structure

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Front-End Developer","Back-End Developer","FullStack Developer","Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
